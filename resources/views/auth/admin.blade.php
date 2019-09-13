7b430dd998c0657f5533852597910e7529c6f094
24628 <32679142+24628@users.noreply.github.com>
2019-09-13 09:38:19 +0200

    added chart for admin panel

--- resources/views/auth/admin.blade.php
+++ resources/views/auth/admin.blade.php
@@ -9,6 +9,8 @@
     </style>
 
 
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0/dist/Chart.min.js"
+            integrity="sha256-Uv9BNBucvCPipKQ2NS9wYpJmi8DTOEfTA/nH2aoJALw=" crossorigin="anonymous"></script>
 
 
 
@@ -16,7 +18,17 @@
 
 @section('content')
 
-
+    <div style="width:75%;">
+        <canvas id="canvas"></canvas>
+    </div>
+    <br>
+    <br>
+    <button id="randomizeData">Randomize Data</button>
+    <button id="addDataset">Add Dataset</button>
+    <button id="removeDataset">Remove Dataset</button>
+    <button id="addData">Add Data</button>
+    <button id="removeData">Remove Data</button>
+    <script src="{{asset('js/chart.js')}}"></script>
 
 
 
