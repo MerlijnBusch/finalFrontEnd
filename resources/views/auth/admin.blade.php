13ea02a7472d04bc5899fec8ad0bd01e63db7508
24628 <32679142+24628@users.noreply.github.com>
2019-09-13 08:55:35 +0200

    front end keuze

---
+++ resources/views/auth/admin.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.master')
+
+@section('jumbotron')
+
+    <style>
+        input[type="text"], input[type="email"], input[type="password"] {
+            background-color: lightgray;
+        }
+    </style>
+
+
+
+
+
+@endsection
+
+@section('content')
+
+
+
+
+
+@endsection
