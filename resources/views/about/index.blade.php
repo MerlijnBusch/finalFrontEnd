374049fce65a045466e6782f6d151b2e51c7b706
24628 <32679142+24628@users.noreply.github.com>
2019-09-27 10:10:28 +0200

    textupdate

--- resources/views/about/index.blade.php
+++ resources/views/about/index.blade.php
@@ -22,7 +22,7 @@
                                     <div class="card-body text-center">
                                         <p><img class=" img-fluid" src="https://sunlimetech.com/portfolio/boot4menu/assets/imgs/team/img_01.png" alt="card image"></p>
                                         <h4 class="card-title">Bernert</h4>
-                                        <p class="card-text">This is basic card with image on top, title, description and button.</p>
+                                        <p class="card-text">Very nice people who will do the best work for oure wine brand trust</p>
                                         <a href="#" class="btn btn-primary btn-sm"><i class="fa fa-plus"></i></a>
                                     </div>
                                 </div>
@@ -70,7 +70,7 @@
                                     <div class="card-body text-center">
                                         <p><img class=" img-fluid" src="https://sunlimetech.com/portfolio/boot4menu/assets/imgs/team/img_02.png" alt="card image"></p>
                                         <h4 class="card-title">Jankie</h4>
-                                        <p class="card-text">This is basic card with image on top, title, description and button.</p>
+                                        <p class="card-text">Very nice people who will do the best work for oure wine brand trust</p>
                                         <a href="#" class="btn btn-primary btn-sm"><i class="fa fa-plus"></i></a>
                                     </div>
                                 </div>
@@ -118,7 +118,7 @@
                                     <div class="card-body text-center">
                                         <p><img class=" img-fluid" src="https://sunlimetech.com/portfolio/boot4menu/assets/imgs/team/img_03.png" alt="card image"></p>
                                         <h4 class="card-title">jacob</h4>
-                                        <p class="card-text">This is basic card with image on top, title, description and button.</p>
+                                        <p class="card-text">Very nice people who will do the best work for oure wine brand trust</p>
                                         <a href="#" class="btn btn-primary btn-sm"><i class="fa fa-plus"></i></a>
                                     </div>
                                 </div>
